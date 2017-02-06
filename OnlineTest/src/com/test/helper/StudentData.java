f646613fd6061d5fb568c82fac2080f45ddb65c9
Shikhar <shikhar.bajaj16@gmail.com>
2017-02-06 18:01:59 +0530

    StudentData

--- OnlineTest/src/com/test/helper/StudentData.java
+++ OnlineTest/src/com/test/helper/StudentData.java
@@ -1,5 +1,32 @@
 package com.test.helper;
 
-public class StudentData {
+import java.util.Scanner;
 
+import com.test.bean.Student;
+public class StudentData {
+private Student student;
+	
+	public void input(){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Username : ");
+		String username=sc.next();
+		System.out.println("Enter Password : ");
+		String password=sc.next();
+		System.out.println("Enter Name : ");
+		String name=sc.next();
+		System.out.println("Enter Phone Number : ");
+		String phone=sc.next();
+		System.out.println("Enter Email : ");
+		String email=sc.next();
+		Student student=new Student(username,password,name,phone,email);
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	
 }
