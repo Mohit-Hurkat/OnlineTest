65282b80cac0dd61842eaffd4abe2cdd6ad7b4f9
mannu219 <mk93137@gmail.com>
2017-02-07 11:05:56 +0530

    Questionlogic committed.

--- OnlineTest/src/com/test/bl/QuestionLogic.java
+++ OnlineTest/src/com/test/bl/QuestionLogic.java
@@ -1,5 +1,41 @@
 package com.test.bl;
 
-public class QuestionLogic {
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
+import com.test.bean.Question;
+import com.test.dao.QuestionDao;
+import com.test.dao.QuestionDaoImpl;
+
+public class QuestionLogic 
+{
+	private QuestionDao qdao=new QuestionDaoImpl();
+	
+	boolean insert(Question question) throws IOException, ClassNotFoundException, SQLException
+	{
+		return qdao.insert(question);
+		
+	}
+	Question search(int question_Id) throws IOException, ClassNotFoundException, SQLException
+	{
+		return qdao.search(question_Id);
+		
+	}
+	List<Question> displayAll(int subjectId) throws IOException, ClassNotFoundException, SQLException
+	{
+		return qdao.displayAll(subjectId);
+		
+	}
+	boolean update(int questionId, Question question) throws IOException, ClassNotFoundException, SQLException
+	{
+		return qdao.update(questionId, question);
+		
+	}
+	boolean delete(int questionID) throws IOException, ClassNotFoundException, SQLException
+	{
+		return qdao.delete(questionID);
+		
+	}
+	
 }
