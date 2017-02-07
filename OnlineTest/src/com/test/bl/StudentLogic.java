31f31e70940f72281f5dbf129232a278f12ed866
priyankmahajan <priyankmahajan04@gmail.com>
2017-02-07 11:10:38 +0530

    created StudentLogic BL

--- OnlineTest/src/com/test/bl/StudentLogic.java
+++ OnlineTest/src/com/test/bl/StudentLogic.java
@@ -1,5 +1,29 @@
 package com.test.bl;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.test.bean.Student;
+import com.test.dao.StudentDao;
+import com.test.dao.StudentDaoImpl;
+
 public class StudentLogic {
+	private StudentDao studentDao = new StudentDaoImpl();
+	public boolean delete(String username) throws IOException, ClassNotFoundException, SQLException{
+		return studentDao.delete(username);
+	}
+	boolean insert(Student student) throws IOException, ClassNotFoundException, SQLException{
+		return studentDao.insert(student);
+	}
+	Student search(String username) throws IOException, ClassNotFoundException, SQLException{
+		return studentDao.search(username);
+	}
+	List<Student> displayAll() throws IOException,ClassNotFoundException, SQLException{
+		return studentDao.displayAll();
+	}
+	boolean update(String username, Student newStudent) throws IOException, ClassNotFoundException, SQLException{
+		return studentDao.update(username, newStudent);
+	}
 
 }
