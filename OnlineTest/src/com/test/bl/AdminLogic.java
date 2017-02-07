a1402f79a5793bb3df5e0a32fc43797be786b992
shikhar bajaj <shikhar.bajaj16@gmail.com>
2017-02-07 11:02:38 +0530

    AdminLogicBL

--- OnlineTest/src/com/test/bl/AdminLogic.java
+++ OnlineTest/src/com/test/bl/AdminLogic.java
@@ -1,5 +1,27 @@
 package com.test.bl;
 
-public class AdminLogic {
+import java.io.IOException;
+import java.sql.SQLException;
 
+import com.test.bean.Admin;
+import com.test.dao.AdminDao;
+import com.test.dao.AdminDaoImpl;
+
+
+
+public class AdminLogic {
+	private AdminDao adminDao=new AdminDaoImpl();
+	public boolean update(String username, Admin admin) throws IOException, ClassNotFoundException, SQLException{
+		return adminDao.update(username, admin);
+	}
+	
+	public Admin retrieveAdminRecord() throws IOException, ClassNotFoundException, SQLException {
+		return adminDao.retrieveAdminRecord();
+	}
+	public boolean insert(Admin admin)throws IOException, ClassNotFoundException, SQLException{
+		return adminDao.insert(admin);
+	}
+	public boolean delete(String username) throws IOException, ClassNotFoundException, SQLException{
+		return adminDao.delete(username);
+	}
 }
