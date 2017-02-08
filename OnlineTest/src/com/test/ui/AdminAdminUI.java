05b3b64ebd0e159fa18aa31cea747809e99796da
mohit <mohit.hurkat@gmail.com>
2017-02-08 12:32:13 +0530

    AdminAdminUI

--- OnlineTest/src/com/test/ui/AdminAdminUI.java
+++ OnlineTest/src/com/test/ui/AdminAdminUI.java
@@ -9,8 +9,7 @@ import com.test.bl.AdminLogic;
 
 public class AdminAdminUI {
 	private AdminLogic adminbl = new AdminLogic();
-	private static final String MENU_OPTIONS_ADMIN = "1. Insert New Admin" +
-			"\n 2. Update Admin Password" + "\n 3. Delete Admin"+ " \n 4. Exit";
+	private static final String MENU_OPTIONS_ADMIN = "1. Update Admin Password" + " \n 2. Exit";
 	private String username;
 	public void displayMenu(){
 		System.out.println(MENU_OPTIONS_ADMIN);
@@ -30,15 +29,6 @@ public class AdminAdminUI {
 			switch(choice)
 			{
 			case 1:
-					System.out.println("Enter UserName:");
-					username=scanner.next();
-					System.out.println("Enter password:");
-					pass=scanner.next();
-					admin=new Admin(username,pass);
-					adminbl.insert(admin);
-				break;
-			case 2:
-				System.out.println(username);
 					admin=adminbl.search(username);
 					System.out.println(admin);
 					username=admin.getUsername();
@@ -46,14 +36,7 @@ public class AdminAdminUI {
 					pass=scanner.next();
 					adminbl.update(username, pass);
 				break;
-			case 3:
-				admin=adminbl.search(username);
-					username=admin.getUsername();
-					System.out.println("Enter Username You Want To Delete:");
-					pass=scanner.next();
-					adminbl.delete(username, pass);
-				break;
-			case 4:
+			case 2:
 					return false;
 			default:
 						System.out.println("Invalid Choice");
