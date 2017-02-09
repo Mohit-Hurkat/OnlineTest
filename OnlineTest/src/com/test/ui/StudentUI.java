f7e089ead51e9e529e3558cef5c70e71b19dc559
mohit <mohit.hurkat@gmail.com>
2017-02-09 11:51:20 +0530

    studentUI

--- OnlineTest/src/com/test/ui/StudentUI.java
+++ OnlineTest/src/com/test/ui/StudentUI.java
@@ -17,10 +17,11 @@ import com.test.helper.StudentData;
 public class StudentUI 
 {
 	private StudentDaoImpl studentDao=new StudentDaoImpl();
+	private StudentData studentD=new StudentData();
 	private TestLogic tbl=new TestLogic();
 	private SubjectLogic sbl=new SubjectLogic();
 	private Student student=null;
-	private static final String STUDENT_MENU_OPTIONS ="1.Update Student Record"+	
+	private static final String STUDENT_MENU_OPTIONS ="1.Update Your Record"+	
 			"\n" + "2.Give Online Test"+	
 			"\n" + "3.Exit"; 
 	private static final String CHOICE_MSG = "Enter your choice";
@@ -46,7 +47,6 @@ public class StudentUI
 		{
 		
 		case 1:
-			StudentData studentD=new StudentData();
 			student=studentD.update();
 			if(student!=null)
 			 studentDao.update(username, student);
@@ -57,19 +57,12 @@ public class StudentUI
 			break;
 		
 		case 2:
+			studentD.instruction();
 			Scanner sc= new Scanner(System.in);
 				List<Subject> subList=sbl.displayAll();
 				for(Subject sub:subList){
 				System.out.println(sub);
 				}
-			System.out.println("\n-------------------INSTRUCTIONS FOR ONLINE EXAM-------------------");
-			System.out.println("1)You will be given only 15 seconds to attempt every question");
-			System.out.println("2)You can't skip any question");
-			System.out.println("3)Every question has 4 options out of which only one is correct");
-			System.out.println("4)You can't attempt the same test twice");
-			System.out.println("5)Once answered you can't change your answer");
-			System.out.println("6)Each question is of one mark");
-			System.out.println("7)Your result will be displayed as per the number of correct questions you have answered in percentage");
 			System.out.println("Enter Subject-Id");
 			int subjectId=sc.nextInt();
 			status=tbl.check(subjectId);
